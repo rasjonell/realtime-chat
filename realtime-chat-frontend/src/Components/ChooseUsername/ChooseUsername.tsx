ea73446c92366cfc320aee8c80d397897cadeea1
Gurgen Hayrapetyan <info.gurgen@gmail.com>
2023-12-03 22:33:06 +0400

    [Frontend] Authenticate by username

--- realtime-chat-frontend/src/Components/ChooseUsername/ChooseUsername.tsx
+++ realtime-chat-frontend/src/Components/ChooseUsername/ChooseUsername.tsx
@@ -3,8 +3,8 @@ import { ChangeEvent, KeyboardEvent, useState } from 'react';
 import { useChatContext } from '../../contexts/ChatContext';
 
 function ChooseUsername() {
-  const { join } = useChatContext();
   const [userName, setUserName] = useState('');
+  const { join, joinError, userName: registeredUserName } = useChatContext();
 
   const handleJoin = () => {
     join(userName);
@@ -32,35 +32,42 @@ function ChooseUsername() {
               Enter a username and connect with friends, family and communities of people and
               dicsuss any topics of interest.
             </p>
-            <div className="flex flex-col space-y-4 sm:flex-row sm:space-y-0 sm:space-x-4">
-              <input
-                value={userName}
-                onKeyDown={handleKeyPress}
-                onChange={handleUserNameChange}
-                placeholder="Enter Your Username"
-                className="inline-flex items-center justify-center px-4 py-2.5 text-base font-medium text-center bg-primary-700 rounded-lg hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
-              />
-              <button
-                onClick={handleJoin}
-                className="inline-flex items-center justify-center px-4 py-2.5 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-              >
-                <svg
-                  fill="none"
-                  aria-hidden="true"
-                  viewBox="0 0 14 10"
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="w-5 h-5 mr-2 text-gray-800 dark:text-white"
+            <div>
+              <div className="flex flex-col space-y-4 sm:flex-row sm:space-y-0 sm:space-x-4">
+                <input
+                  value={userName}
+                  onKeyDown={handleKeyPress}
+                  onChange={handleUserNameChange}
+                  placeholder="Enter Your Username"
+                  className="inline-flex items-center justify-center px-4 py-2.5 text-base font-medium text-center bg-primary-700 rounded-lg hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
+                />
+                <button
+                  onClick={handleJoin}
+                  className="inline-flex items-center justify-center px-4 py-2.5 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
                 >
-                  <path
-                    strokeWidth="2"
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M1 5h12m0 0L9 1m4 4L9 9"
-                  />
-                </svg>
-                Join!
-              </button>
+                  <svg
+                    fill="none"
+                    aria-hidden="true"
+                    viewBox="0 0 14 10"
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-5 h-5 mr-2 text-gray-800 dark:text-white"
+                  >
+                    <path
+                      strokeWidth="2"
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M1 5h12m0 0L9 1m4 4L9 9"
+                    />
+                  </svg>
+                  Join!
+                </button>
+              </div>
+              {joinError ? (
+                <p className="text-red-300 mt-3">
+                  Username: "{registeredUserName}" is already registered.
+                </p>
+              ) : null}
             </div>
           </div>
         </div>
--- realtime-chat-frontend/src/contexts/ChatContext.tsx
+++ realtime-chat-frontend/src/contexts/ChatContext.tsx
@@ -13,14 +13,25 @@ export type Message = {
   createdAt: string;
 };
 
-type UsersChangedEventData = {
-  user: User;
-  event: 'joined' | 'left';
+type NewcomerData = {
+  users: User[];
+  messages: Message[];
 };
+
+type UsersChangedEventData =
+  | {
+      user: User;
+      event: 'joined';
+    }
+  | {
+      event: 'left';
+      user: User | null;
+    };
 
 type ChatContextData = {
   users: User[];
   userName: string;
+  joinError: boolean;
   messages: Message[];
   isConnected: boolean;
   join: (userName: string) => void;
@@ -32,6 +43,7 @@ export const ChatContext = createContext<ChatContextData>({
   userName: '',
   messages: [],
   join: () => {},
+  joinError: false,
   isConnected: false,
   sendMessage: () => {},
 });
@@ -39,52 +51,64 @@ export const ChatContext = createContext<ChatContextData>({
 export const ChatContextProvider = ({ children }: PropsWithChildren) => {
   const [userName, setUserName] = useState('');
   const [users, setUsers] = useState<User[]>([]);
+  const [joinError, setJoinError] = useState(false);
   const [messages, setMessages] = useState<Message[]>([]);
 
   const [isConnected, setIsConnected] = useState(false);
 
-  const join = (userName: string) => {
-    console.log('joining');
-
+  const join = (newUserName: string) => {
+    setUserName(newUserName);
     socket.connect();
-    socket.emit('join', userName);
-    setIsConnected(true);
-    setUserName(userName);
+    setJoinError(false);
+    socket.emit('join', newUserName);
   };
 
   const sendMessage = (message: string): void => {
-    console.log('sending message', message);
-
     socket.emit('addNewMessage', message);
   };
 
   const onConnect = (): void => {
-    console.log('onConnect');
+    console.info('onConnect');
   };
 
   const onDisconnect = (): void => {
-    console.log('onDisconnect');
+    setUserName('');
+    setJoinError(false);
     setIsConnected(false);
   };
 
   const onMessageFromServer = (data: Message): void => {
-    console.log('onMessageFromServer data', data);
     setMessages((prev) => [...prev, data]);
   };
 
+  const onUserExists = () => {
+    setJoinError(true);
+    setIsConnected(false);
+  };
+
   const onUsersChanged = (data: UsersChangedEventData): void => {
-    console.log('onUsers data', data);
     // show toast
     if (data.event === 'joined') {
       setUsers((prev) => [...prev, data.user]);
-    } else {
-      setUsers((prev) => prev.filter((user) => user.userName !== data.user.userName));
+    } else if (data.user) {
+      setUsers((prev) => prev.filter((user) => user.userName !== data.user?.userName));
     }
   };
 
-  const onConnectionEstablished = (data: any): void => {
-    console.log('onConnectionEstablished', data);
+  const onJoined = (data: NewcomerData): void => {
+    setUsers((prev) => [...prev, ...data.users]);
+    setMessages((prev) => [...prev, ...data.messages]);
   };
+
+  const onConnectionEstablished = (): void => {
+    console.info('onConnectionEstablished');
+  };
+
+  useEffect(() => {
+    if (!joinError && users.find((user) => user.userName === userName)) {
+      setIsConnected(true);
+    }
+  }, [userName, users, joinError]);
 
   useEffect(() => {
     // WS Connection Events
@@ -92,6 +116,8 @@ export const ChatContextProvider = ({ children }: PropsWithChildren) => {
     socket.on('disconnect', onDisconnect);
 
     // RealTime Chat Events
+    socket.on('joined', onJoined);
+    socket.on('userExists', onUserExists);
     socket.on('usersChanged', onUsersChanged);
     socket.on('message', onMessageFromServer);
     socket.on('connected', onConnectionEstablished);
@@ -102,14 +128,18 @@ export const ChatContextProvider = ({ children }: PropsWithChildren) => {
       socket.off('disconnect', onDisconnect);
 
       // RealTime Chat Events
+      socket.off('joined', onJoined);
+      socket.off('userExists', onUserExists);
       socket.off('usersChanged', onUsersChanged);
       socket.off('message', onMessageFromServer);
       socket.off('connected', onConnectionEstablished);
     };
-  }, []);
+  }, [socket]);
 
   return (
-    <ChatContext.Provider value={{ join, sendMessage, userName, users, messages, isConnected }}>
+    <ChatContext.Provider
+      value={{ join, sendMessage, joinError, userName, users, messages, isConnected }}
+    >
       {children}
     </ChatContext.Provider>
   );
