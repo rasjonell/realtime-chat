7c9db95f5b2052e3ce26a0c9d0cdfb8f5ce24709
Gurgen Hayrapetyan <info.gurgen@gmail.com>
2023-12-04 03:27:54 +0400

    [Backend] Enable CORS for the frontend

--- realtime-chat-backend/src/main.ts
+++ realtime-chat-backend/src/main.ts
@@ -3,7 +3,10 @@ import { NestFactory } from '@nestjs/core';
 import { ChatModule } from './chat.module';
 
 async function bootstrap() {
-  const chatApp = await NestFactory.create(ChatModule);
+  const chatApp = await NestFactory.create(ChatModule, {
+    cors: { origin: 'http://localhost:5173' },
+  });
+
   await chatApp.listen(3000);
 }
 bootstrap();
